edd739f1aeabf1649ff2ea50073c47f9d1a27f2e
Monica Marziano <mmarziano1@gmail.com>
2019-02-25 00:25:33 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -48,8 +48,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  binding.pry
-  movie = Movie.find.first
+  Movie.find
 end
 
 def can_find_by_multiple_attributes
